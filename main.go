13bb48333fd575e67d641f4c376b18488b08d8a3
Derek Smith <drsmith.phys@gmail.com>
2022-01-21 16:42:12 -0600

    chore: adjust copyright date
    Signed-off-by: Derek Smith <drsmith.phys@gmail.com>

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "cidr"
-	app.Copyright = "(c) 2021 Derek Smith"
+	app.Copyright = "(c) 2022 Derek Smith"
 	app.Authors = []*cli.Author{
 		{
 			Name:  "Derek Smith",
